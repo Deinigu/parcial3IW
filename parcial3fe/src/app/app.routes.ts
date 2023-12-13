6973af6ce231ae74089a39bf3506961372fa146c
Deinigu <diegolopezreduello@gmail.com>
2023-12-14 00:18:12 +0100

    ✨Inicio

--- parcial3fe/src/app/app.routes.ts
+++ parcial3fe/src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { ImagenesComponent } from './features/imagenes/imagenes.component';
+import { InicioComponent } from './features/inicio/inicio.component';
 
 export const routes: Routes = [
+    {
+        path : '',
+        component: InicioComponent,
+        title : 'Inicio'
+    },
     {
         path : 'imagen',
         component : ImagenesComponent,
