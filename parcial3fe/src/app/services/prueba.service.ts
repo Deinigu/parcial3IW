ff1d966a1d05885950eb7d6136b3b9361720cc47
Deinigu <diegolopezreduello@gmail.com>
2023-12-15 19:47:58 +0100

    😀Cloud

--- parcial3fe/src/app/services/prueba.service.ts
+++ parcial3fe/src/app/services/prueba.service.ts
@@ -10,32 +10,32 @@ export class PruebaService {
   constructor(private http: HttpClient) {}
 
   getAllPruebas(): Observable<any> {
-    const url = 'http://localhost:8000/api/prueba/';
+    const url = 'http://13.38.223.212:8000/api/prueba/';
     return this.http.get<any>(url);
   }
 
   getPruebaInfo(idPrueba: string): Observable<any> {
-    const url = 'http://localhost:8000/api/prueba/' + idPrueba + '/';
+    const url = 'http://13.38.223.212:8000/api/prueba/' + idPrueba + '/';
     return this.http.get<any>(url);
   }
 
   createPrueba(prueba: Prueba): Observable<any> {
-    const url = 'http://localhost:8000/api/prueba/';
+    const url = 'http://13.38.223.212:8000/api/prueba/';
     return this.http.post<any>(url, prueba);
   }
 
   editPrueba(idPrueba: string, prueba: Prueba): Observable<any> {
-    const url = 'http://localhost:8000/api/prueba/' + idPrueba + '/';
+    const url = 'http://13.38.223.212:8000/api/prueba/' + idPrueba + '/';
     return this.http.put<any>(url, prueba);
   }
 
   deletePrueba(idPrueba: string): Observable<any> {
-    const url = 'http://localhost:8000/api/prueba/' + idPrueba + '/';
+    const url = 'http://13.38.223.212:8000/api/prueba/' + idPrueba + '/';
     return this.http.delete<any>(url);
   }
 
   buscarPorLongLat(direccion : string): Observable<any>{
-    const url = 'http://localhost:8000/api/prueba/busquedaLatLot/' + direccion + '/';
+    const url = 'http://13.38.223.212:8000/api/prueba/busquedaLatLot/' + direccion + '/';
     return this.http.get<any>(url);
   }
 
